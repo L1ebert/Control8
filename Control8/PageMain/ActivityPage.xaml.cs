5b0b80654e29671d238b10420edb9e0a307fc1ae
Николай Толмачев <niktolm556@gmail.com>
2024-01-30 12:07:21 +0300

    Добавьте файлы проекта.

---
+++ Control8/PageMain/ActivityPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Control8.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Control8.PageMain
+{
+    /// <summary>
+    /// Логика взаимодействия для ActivityPage.xaml
+    /// </summary>
+    public partial class ActivityPage : Page
+    {
+        public ActivityPage()
+        {
+            InitializeComponent();
+
+            DirectionCmb.SelectedValuePath = "ID";
+            DirectionCmb.DisplayMemberPath = "Name";
+            DirectionCmb.ItemsSource = App.context.Direction.ToList();
+        }
+
+        private void AddActivitylBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (NameActiviryTb.Text == "")
+            {
+                MessageBox.Show("Введите название активности");
+            }
+            if (DirectionCmb.Text == "")
+            {
+                MessageBox.Show("Выберите активность");
+            }
+            Activity addActivity = new Activity
+            {
+                Name = NameActiviryTb.Text,
+                Direction = DirectionCmb.SelectedItem as Direction
+            };
+            App.context.Activity.Add(addActivity);
+            App.context.SaveChanges();
+            MessageBox.Show("Активность добавлена");
+            NameActiviryTb.Text = "";
+            DirectionCmb.Text = "";
+        }
+    }
+}
---
+++ Control8/PageMain/AddGroupPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Control8.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Control8.PageMain
+{
+    /// <summary>
+    /// Логика взаимодействия для AddGroupPage.xaml
+    /// </summary>
+    public partial class AddGroupPage : Page
+    {
+        public AddGroupPage()
+        {
+            InitializeComponent();
+
+            SpecialCmb.SelectedValuePath = "Code";
+            SpecialCmb.DisplayMemberPath = "Name";
+            SpecialCmb.ItemsSource = App.context.Special.ToList();
+        }
+
+        private void AddGrouplBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(NameGroupTb.Text == "")
+            {
+                MessageBox.Show("Введите наименование группы");
+            }
+            if(SpecialCmb.Text == "")
+            {
+                MessageBox.Show("Выберите специальность");
+            }
+            Group addGroup = new Group
+            {
+                Name = NameGroupTb.Text,
+                Special = SpecialCmb.SelectedItem as Special
+            };
+            App.context.Group.Add(addGroup);
+            App.context.SaveChanges();
+            MessageBox.Show("Группа добавлена!");
+            NameGroupTb.Text = "";
+            SpecialCmb.Text = "";
+        }
+    }
+}
